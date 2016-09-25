4438f165f1b78418e6a4d49b1be26f77a01a0a48
ct16k <thciobanu@yahoo.com>
2016-09-26 00:08:20 +0300

    + use fadvise if available to lower cache pressure * configurable main and secondary hash - weakhash option removed, no longer needed * some python3 suport

--- blocksync.py
+++ blocksync.py
@@ -25,25 +25,45 @@ from __future__ import print_function
 import os
 import sys
 import signal
-from hashlib import sha512, sha384, sha1, md5
+import hashlib
 from math import ceil
 import subprocess
 import time
 from datetime import timedelta
 
-SAME = "0"
-DIFF = "1"
+SAME = b"0"
+DIFF = b"1"
 COMPLEN = len(SAME)  # SAME/DIFF length
+
+if callable(getattr(os, "posix_fadvise", False)):
+    from os import POSIX_FADV_NORMAL, POSIX_FADV_SEQUENTIAL, POSIX_FADV_RANDOM, \
+        POSIX_FADV_NOREUSE, POSIX_FADV_WILLNEED, POSIX_FADV_DONTNEED
+    posix_fadvise = lambda fileobj, offset, length, advice: os.posix_fadvise(fileobj.fileno(), offset, length, advice)
+else:
+    try:
+        from fadvise import POSIX_FADV_NORMAL, POSIX_FADV_SEQUENTIAL, POSIX_FADV_RANDOM, \
+            POSIX_FADV_NOREUSE, POSIX_FADV_WILLNEED, POSIX_FADV_DONTNEED
+        posix_fadvise = lambda fileobj, offset, length, advice: fadvise.set_advice(fileobj, advice, offset, length)
+    except:
+        posix_fadvise = None
+
+if posix_fadvise:
+    USE_DONTNEED = sys.platform.startswith('linux')
+    USE_NOREUSE = not(USE_DONTNEED)
+else:
+    USE_DONTNEED = USE_NOREUSE = False
 
 
 def do_create(f, size):
-    f = open(f, 'a')
+    f = open(f, 'a', 0)
     f.truncate(size)
     f.close()
 
 
 def do_open(f, mode):
     f = open(f, mode)
+    if USE_NOREUSE:
+        posix_fadvise(f, 0, 0, POSIX_FADV_NOREUSE)
     f.seek(0, 2)
     size = f.tell()
     f.seek(0)
@@ -55,20 +75,26 @@ def getblocks(f, blocksize):
         block = f.read(blocksize)
         if not block:
             break
+        if USE_DONTNEED:
+            posix_fadvise(f, f.tell() - blocksize, blocksize, POSIX_FADV_DONTNEED)
         yield block
 
 
 def server(dev, deleteonexit, options):
-    blocksize, addhash = options.blocksize, options.addhash
+    blocksize = options.blocksize
 
-    if options.weakhash:
-        hash1 = sha1
-        hash2 = md5
-    else:
-        hash1 = sha512
-        hash2 = sha384
+    hash1 = getattr(hashlib, options.hash.lower())
+    hash2 = getattr(hashlib, options.addhash.lower()) if options.addhash else False
 
     print('init')
+    sys.stdout.flush()
+
+    if USE_NOREUSE:
+        print('NOREUSE')
+    elif USE_DONTNEED:
+        print('DONTNEED')
+    else:
+        print('None')
     sys.stdout.flush()
 
     size = int(sys.stdin.readline().strip())
@@ -76,7 +102,7 @@ def server(dev, deleteonexit, options):
         do_create(dev, size)
 
     print(dev, blocksize)
-    f, size = do_open(dev, 'r+')
+    f, size = do_open(dev, 'rb+')
     print(size)
     sys.stdout.flush()
 
@@ -85,16 +111,26 @@ def server(dev, deleteonexit, options):
 
     f.seek(startpos)
 
+    if getattr(sys.stdin, "buffer", False):
+        stdin = sys.stdin.buffer
+        stdout = sys.stdout.buffer
+    else:
+        stdin = sys.stdin
+        stdout = sys.stdout
+
     for i, block in enumerate(getblocks(f, blocksize)):
-        sys.stdout.write(hash1(block).digest())
-        if addhash:
-            sys.stdout.write(hash2(block).digest())
-        sys.stdout.flush()
-        res = sys.stdin.read(COMPLEN)
+        stdout.write(hash1(block).digest())
+        if hash2:
+            stdout.write(hash2(block).digest())
+        stdout.flush()
+        res = stdin.read(COMPLEN)
         if res == DIFF:
-            newblock = sys.stdin.read(blocksize)
-            f.seek(-len(newblock), 1)
+            newblock = stdin.read(blocksize)
+            newblocklen = len(newblock)
+            f.seek(-newblocklen, 1)
             f.write(newblock)
+            if USE_DONTNEED:
+                posix_fadvise(f, f.tell() - newblocklen, newblocklen, POSIX_FADV_DONTNEED)
         if i == maxblock:
             break
 
@@ -115,7 +151,7 @@ def copy_self(workerid, remotecmd):
         print("[worker %d] Error copying blocksync to the remote host!" % workerid, file = options.outfile)
         sys.exit(1)
 
-    return remotescript
+    return remotescript.decode('UTF-8')
 
 
 def sync(workerid, srcdev, dsthost, dstdev, options):
@@ -127,21 +163,12 @@ def sync(workerid, srcdev, dsthost, dstdev, options):
     if not dstdev:
         dstdev = srcdev
 
-    if options.weakhash:
-        hash1 = sha1
-        hash2 = md5
-    else:
-        hash1 = sha512
-        hash2 = sha384
-    hash1len = hash1().digestsize
-    hash2len = hash2().digestsize
-
     print("Starting worker #%d (pid: %d)" % (workerid, os.getpid()), file = options.outfile)
     print("[worker %d] Block size is %0.1f MB" % (workerid, blocksize / (1024.0 * 1024)), file = options.outfile)
 
     try:
-        f, size = do_open(srcdev, 'r')
-    except Exception, e:
+        f, size = do_open(srcdev, 'rb')
+    except Exception as e:
         print("[worker %d] Error accessing source device! %s" % (workerid, e), file = options.outfile)
         sys.exit(1)
 
@@ -156,6 +183,16 @@ def sync(workerid, srcdev, dsthost, dstdev, options):
         # sleep() wants seconds...
         pause_ms = options.pause / 1000.0
         print("[worker %d] Slowing down for %d ms/block (%0.4f sec/block)" % (workerid, options.pause, pause_ms), file = options.outfile)
+
+    hash1 = getattr(hashlib, options.hash.lower())
+    hash1len = hash1().digest_size
+    print("[worker %d] Hash 1: %s" % (workerid, options.hash.lower()), file = options.outfile)
+    if options.addhash:
+        hash2 = getattr(hashlib, options.addhash.lower())
+        hash2len = hash2().digest_size
+        print("[worker %d] Hash 2: %s" % (workerid, options.addhash.lower()), file = options.outfile)
+    else:
+        hash2 = False
 
     cmd = []
     if dsthost != 'localhost':
@@ -184,27 +221,35 @@ def sync(workerid, srcdev, dsthost, dstdev, options):
 
     cmd += [options.interpreter, remotescript, servercmd, dstdev, '-b', str(blocksize)]
 
-    if addhash:
-        cmd += ['-2']
-
-    if options.weakhash:
-        cmd += ['-W']
+    cmd += ['-1', options.hash]
+    if options.addhash:
+        cmd += ['-2', options.addhash]
 
     print("[worker %d] Running: %s" % (workerid, " ".join(cmd[2 if options.passenv and (dsthost != 'localhost') else 0:])), file = options.outfile)
 
     p = subprocess.Popen(cmd, bufsize=0, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
     p_in, p_out = p.stdin, p.stdout
 
-    line = p_out.readline()
+    line = p_out.readline().decode('UTF-8')
     p.poll()
     if (p.returncode is not None) or (line.strip() != 'init'):
         print("[worker %d] Error connecting to or invoking blocksync on the remote host!" % workerid, file = options.outfile)
         sys.exit(1)
 
-    p_in.write("%d\n" % (size if options.createdest else 0))
+    if USE_NOREUSE:
+        fadv = "NOREUSE"
+    elif USE_DONTNEED:
+        fadv = "DONTNEED"
+    else:
+        fadv = "None"
+    print("[worker %d] Local fadvise: %s" % (workerid, fadv), file = options.outfile)
+    fadv = p_out.readline().decode('UTF-8').strip()
+    print("[worker %d] Remote fadvise: %s" % (workerid, fadv), file = options.outfile)
+
+    p_in.write(bytes(("%d\n" % (size if options.createdest else 0)).encode("UTF-8")))
     p_in.flush()
 
-    line = p_out.readline()
+    line = p_out.readline().decode('UTF-8')
     p.poll()
     if p.returncode is not None:
       print("[worker %d] Failed creating destination file on the remote host!" % workerid, file = options.outfile)
@@ -218,7 +263,7 @@ def sync(workerid, srcdev, dsthost, dstdev, options):
         print("[worker %d] Source block size (%d) doesn't match with the remote host (%d)!" % (workerid, blocksize, int(b)), file = options.outfile)
         sys.exit(1)
 
-    line = p_out.readline()
+    line = p_out.readline().decode('UTF-8')
     p.poll()
     if p.returncode is not None:
         print("[worker %d] Error accessing device on remote host!" % workerid, file = options.outfile)
@@ -237,13 +282,13 @@ def sync(workerid, srcdev, dsthost, dstdev, options):
     t_last = t0
     f.seek(startpos)
     size_blocks = ceil(chunksize / float(blocksize))
-    p_in.write("%d\n%d\n" % (startpos, size_blocks))
+    p_in.write(bytes(("%d\n%d\n" % (startpos, size_blocks)).encode("UTF-8")))
     p_in.flush()
     print("[worker %d] Start syncing %d blocks..." % (workerid, size_blocks), file = options.outfile)
     for l_block in getblocks(f, blocksize):
         l1_sum = hash1(l_block).digest()
         r1_sum = p_out.read(hash1len)
-        if addhash:
+        if hash2:
             l2_sum = hash2(l_block).digest()
             r2_sum = p_out.read(hash2len)
             r2_match = (l2_sum == r2_sum)
@@ -295,8 +340,8 @@ if __name__ == "__main__":
     parser.add_option("-w", "--workers", dest = "workers", type = "int", help = "number of workers to fork (defaults to 1)", default = 1)
     parser.add_option("-l", "--splay", dest = "splay", type = "int", help = "sleep between creating workers (ms, defaults to 0)", default = 0)
     parser.add_option("-b", "--blocksize", dest = "blocksize", type = "int", help = "block size (bytes, defaults to 1MB)", default = 1024 * 1024)
-    parser.add_option("-2", "--additionalhash", dest = "addhash", action = "store_true", help = "use two message digests when comparing blocks", default = False)
-    parser.add_option("-W", "--weakhash", dest = "weakhash", action = "store_true", help = "use weaker but faster message digests (SHA1[+MD5] instead of SHA512[+SHA384])", default = False)
+    parser.add_option("-1", "--hash", dest = "hash", help = "hash used for block comparison (defaults to \"sha512\")", default = "sha512")
+    parser.add_option("-2", "--additionalhash", dest = "addhash", help = "second hash used for extra comparison (default is none)")
     parser.add_option("-p", "--pause", dest = "pause", type="int", help = "pause between processing blocks, reduces system load (ms, defaults to 0)", default = 0)
     parser.add_option("-c", "--cipher", dest = "cipher", help = "cipher specification for SSH (defaults to blowfish)", default = "blowfish")
     parser.add_option("-C", "--compress", dest = "compress", action = "store_true", help = "enable compression over SSH (defaults to on)", default = True)
@@ -352,7 +397,7 @@ if __name__ == "__main__":
             # sleep() wants seconds...
             splay_ms = options.splay / 1000.0
         workers = {}
-        for i in xrange(options.workers):
+        for i in range(options.workers):
             pid = os.fork()
             if pid == 0:
                 sync(i, srcdev, dsthost, dstdev, options)
@@ -362,7 +407,7 @@ if __name__ == "__main__":
             if splay_ms:
                 time.sleep(splay_ms)
 
-        for i in xrange(options.workers):
+        for i in range(options.workers):
             pid, err = os.wait()
             print("Worker #%d exited with %d" % (workers[pid], err), file = options.outfile)
             if (err != 0) and not aborting:
